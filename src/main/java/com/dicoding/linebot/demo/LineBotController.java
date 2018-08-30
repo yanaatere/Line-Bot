64254b89b41ccfa0961342c6802de3e39e98f4aa
Yana Andika <yanaandika@gmail.com>
2018-08-30 22:14:49 +0700

    Reply Message

--- src/main/java/com/dicoding/linebot/demo/LineBotController.java
+++ src/main/java/com/dicoding/linebot/demo/LineBotController.java
@@ -1,22 +1,29 @@
 package com.dicoding.linebot.demo;
 
+import java.util.concurrent.ExecutionException;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.client.LineSignatureValidator;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
- 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
- 
+
 @RestController
 public class LineBotController {
  
@@ -27,6 +34,11 @@ public class LineBotController {
     @Autowired
     @Qualifier("lineSignatureValidator")
     private LineSignatureValidator lineSignatureValidator;
+    
+    @RequestMapping(value="/tes", method=RequestMethod.POST)
+    public ResponseEntity<String> tes(){
+    	return new ResponseEntity<String>("Berhasil",HttpStatus.OK);
+    }
  
     @RequestMapping(value="/webhook", method= RequestMethod.POST)
     public ResponseEntity<String> callback(
@@ -42,8 +54,13 @@ public class LineBotController {
             ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
             EventsModel eventsModel = objectMapper.readValue(eventsPayload, EventsModel.class);
             System.out.println("Ini Kepanggil");
+            
             eventsModel.getEvents().forEach((event)->{
-                
+            	if (event instanceof MessageEvent) {
+                    MessageEvent messageEvent = (MessageEvent) event;
+                    TextMessageContent textMessageContent = (TextMessageContent) messageEvent.getMessage();
+                    replyText(messageEvent.getReplyToken(), textMessageContent.getText());
+                }
             });
  
             return new ResponseEntity<>(HttpStatus.OK);
@@ -51,5 +68,25 @@ public class LineBotController {
             e.printStackTrace();
             return new ResponseEntity<String>("Error",HttpStatus.BAD_REQUEST);
         }
+    }
+    
+    private void reply(ReplyMessage replyMessage) {
+    	try {
+			lineMessagingClient.replyMessage(replyMessage).get();
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+    }
+    
+    private void replyText(String replyToken, String messageToUser) {
+    	TextMessage textMessage = new TextMessage(messageToUser);
+    	ReplyMessage replyMessage = new ReplyMessage(replyToken, textMessage);
+    	reply(replyMessage);
+    }
+    
+    private void replySticker(String replyToken, String packageId, String stickerId) {
+    	StickerMessage stickerMessage = new StickerMessage(packageId, stickerId);
+    	ReplyMessage replyMessage = new ReplyMessage(replyToken, stickerMessage);
+    	reply(replyMessage);
     }
 }
